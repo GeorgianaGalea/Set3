d533761f3dd7d3de0a7874afbd10aeeaf15a8f19
GeorgianaGalea <147001550+GeorgianaGalea@users.noreply.github.com>
2024-01-20 16:50:24 +0200

    Add files via upload

---
+++ Setul3_29/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Setul3_29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti lungimea vectorilor: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] E = new int[n];
+            int[] W = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Introduceti elementul E[{i}]: ");
+                E[i] = int.Parse(Console.ReadLine());
+                Console.Write($"Introduceti ponderea pentru E[{i}]: ");
+                W[i] = int.Parse(Console.ReadLine());
+            }
+            SortareBicriteriala(E, W);
+            Console.WriteLine("Vectorii sortati:");
+            AfiseazaVectori(E, W);
+        }
+        static void SortareBicriteriala(int[] E, int[] W)
+        {
+            int lungime = E.Length;
+            for (int i = 0; i < lungime - 1; i++)
+            {
+                for (int j = i + 1; j < lungime; j++)
+                {
+                    if (E[i] > E[j] || (E[i] == E[j] && W[i] < W[j]))
+                    {
+                        SchimbaElemente(E, i, j);
+                        SchimbaElemente(W, i, j);
+                    }
+                }
+            }
+        }
+        static void SchimbaElemente(int[] vector, int index1, int index2)
+        {
+            int temp = vector[index1];
+            vector[index1] = vector[index2];
+            vector[index2] = temp;
+        }
+        static void AfiseazaVectori(int[] E, int[] W)
+        {
+            for (int i = 0; i < E.Length; i++)
+            {
+                Console.WriteLine($"E[{i}] = {E[i]}, W[{i}] = {W[i]}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Setul3_30/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Setul3_30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti lungimea vectorului: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            Console.WriteLine("Introduceti elementele vectorului:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Elementul {i + 1}: ");
+                v[i] = int.Parse(Console.ReadLine());
+            }
+            int majoritate = GasesteElementMajoritate(v);
+            if (majoritate != -1)
+            {
+                Console.WriteLine($"Elementul majoritate este: {majoritate}");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Nu exista element majoritate.");
+                Console.ReadLine();
+            }
+        }
+        static int GasesteElementMajoritate(int[] vector)
+        {
+            int majoritateCandidat = -1;
+            int count = 0;
+            foreach (int element in vector)
+            {
+                if (count == 0)
+                {
+                    majoritateCandidat = element;
+                    count = 1;
+                }
+                else if (element == majoritateCandidat)
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+            count = 0;
+            foreach (int element in vector)
+            {
+                if (element == majoritateCandidat)
+                {
+                    count++;
+                }
+            }
+            return count > vector.Length / 2 ? majoritateCandidat : -1;
+        }
+    }
+}
